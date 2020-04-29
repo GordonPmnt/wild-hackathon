b1cd4ed0dd46fa919751f0a0f9426d3b1bf9bc4e
BasileRaiwet <basileraiwet.pro@gmail.com>
2020-04-29 13:22:03 +0200

    init map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MapContainer from './components/MapContainer';
 
 function App() {
   return (
     <div className="App">
-      
+      <MapContainer />
     </div>
   );
 }
