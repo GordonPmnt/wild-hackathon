6e13315f540b3a478b261049c5ce5e84b2ca1889
Tiesco2 <dtiekoura@yahoo.fr>
2020-04-29 16:04:24 +0200

    message

---
+++ src/components/Message.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+const messageStyle = {
+    height: "100vh",
+}
+
+class Message extends Component {
+    render(){
+     return(
+         <div style = {messageStyle}>  </div>
+     );
+}
+}
+
+
+export default Message
