07fbd1724a68db4fb99a962829f269be1e9bf49f
Gordon Preumont <preumontgr@gmail.com>
2020-04-29 15:16:09 +0200

    specific webcam request

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
 
     axios
     .get(
-      `${baseUrl}/${location}/${property}/${limit}?show=webcams:location,image&key=${config.API_KEY_WINDY}`
+      `${baseUrl}/${location}/${property}/${limit}?show=webcams:location,image,player&key=${config.API_KEY_WINDY}`
     )
     .then(
       response => {
@@ -25,9 +25,22 @@ class App extends React.Component {
     .catch(error => console.log(error))
   }
 
+  getWebcam = webcam => {
+    const baseUrl = 'https://api.windy.com/api/webcams/v2/list'
+    const webcamRef = `webcam=${webcam}`
+
+    axios
+    .get(
+      `${baseUrl}/${webcamRef}?show=webcams:location,image,player&key=${config.API_KEY_WINDY}`
+    )
+    .then(
+      response => console.log(response.data)
+    )
+  }
+
   componentDidMount = () => {
-    const loc = { lat: 42.44, lng: 3.14, radius: 200 } //for test purpose only
-    this.getNearbyWebcams(loc)                         //for test purpose only
+    this.getNearbyWebcams({ lat: 42.44, lng: 3.14, radius: 200 }) //for test purpose only
+    this.getWebcam("1576343988")                                  //for test purpose only
   }
 
   render () {
