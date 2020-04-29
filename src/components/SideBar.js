7b72a78b4be1d9a07578d271f1e9b55c7c73b549
Gordon Preumont <preumontgr@gmail.com>
2020-04-30 01:23:36 +0200

    design map

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -6,21 +6,26 @@ import MyCam from './MyCam'
 
 const SideBar = ({ choosenCam, toggleView, postcardView }) => {
     const styles = {
-        container: {
+        side: {
             backgroundColor: 'rgba(255, 255, 255, 0.6)',
             padding: '2.5%',
             borderRadius: '12px',
             position: 'absolute',
             height: '80%',
-            width: postcardView ? '90vw' : '30vw',
+            width: '30vw',
             margin: '0',
             top: '10vh',
             right: '5vw'
         },
+        full: {
+            height: '100vh',
+            width: '100vw',
+            margin: '0',
+        }
     }
 
     return (
-        <div style={styles.container}>
+        <div style={postcardView ? styles.full : styles.side}>
             <WindyCam
                 choosenCam={choosenCam}
                 postcardView={postcardView}
