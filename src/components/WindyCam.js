d1aedd32c5f1de678dbe51b528c57202100c6910
Gordon Preumont <preumontgr@gmail.com>
2020-04-29 19:52:42 +0200

    solve conflict

--- src/components/WindyCam.js
+++ src/components/WindyCam.js
@@ -3,20 +3,27 @@ import React from 'react';
 
 const WindyCam = () => {
     const styles = {
-        container: {
-            height: '80vh',
-            width: '30vw',
-            margin: '10vh 2.5vw 10vh 70vw',
-            display: 'flex',
-        },
+        cam: {
+            width: '100%',
+            height: '50%',
+            borderRadius: '15px',
+        }
     }
+
+    const { id, location, player } = choosenCam
+    console.log('CAM', choosenCam)
+
     return (
-        <video controls width="250">
-            <source 
-                src="/media/examples/flower.webm"
-                type="video/webm"
+        <>
+        {choosenCam.player &&
+            <iframe 
+                style={styles.cam}
+                id={id}
+                title={location.city}
+                src={player.live.embed}
             />
-        </video>
+        }
+        </>
     )
 }
 
