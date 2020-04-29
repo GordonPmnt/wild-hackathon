2e0ea64363d30f61225e57e890cd649928e1ab38
Gordon Preumont <preumontgr@gmail.com>
2020-04-29 17:29:09 +0200

    solve conflict

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React from 'react'
 
 
-const Form = () => {
-    return <div></div>
-}
-
-export default Form;
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          value: 'Écrivez un essai à propos de votre élément du DOM préféré'
+        };
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        alert('Un essai a été envoyé : ' + this.state.value);
+        event.preventDefault();
+      }
+    
+      render() {
+        return (
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Essay:
+              <textarea value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <button onClick={this.handleSubmit.bind(this)}>Send</button>
+          </form>
+       
+        );
+      }
+    }
+    
+export default Form
