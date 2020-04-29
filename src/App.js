a6e556ec572315b9727c8994b396e4c670c04296
Gordon Preumont <preumontgr@gmail.com>
2020-04-29 14:18:19 +0200

    getnearbycams

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import axios from 'axios';
+import { config } from './config';
 
-class App {
-  
+class App extends React.Component {
+
+  getNearbyWebcams = ({ lat, lng, radius }) => {
+    const baseUrl = 'https://api.windy.com/api/webcams/v2/list'
+    const location = `nearby=${lat},${lng},${radius}`
+    const property = 'property=live,hd'
+
+    axios
+    .get(
+      `${baseUrl}/${location}/${property}?key=${config.API_KEY_WINDY}`
+    )
+    .then(
+      response => {
+        const { webcams } = response.data.result
+        console.log(webcams)
+        return webcams
+      }
+    )
+    .catch(error => console.log(error))
+  }
+
+  componentDidMount = () => {
+    const loc = { lat: 42.44, lng: 3.14, radius: 100 }
+    this.getNearbyWebcams(loc)
+  }
 
   render () {
     return (
